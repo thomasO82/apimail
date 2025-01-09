5b63c16876fb3163411a5b3a149a5a574513a8c4
thomasO82 <thomas.oliver@ri7.fr>
2025-01-09 18:43:00 +0100

    sfsklfjsdjldf

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+require('dotenv').config()
+const mailer = require('./services/mails');
+const app = express();
+
+app.use(express.json());
+
+
+app.listen(process.env.PORT, (err) => {
+    if (err) {
+        console.log(err);   
+        
+    }else{
+        console.log('Server is running on port', process.env.PORT);
+    }
+});
+
+
+app.get('/sendmail', (req, res) => {
+    try {
+        mailer.sendMail({
+            from: req.body.from,
+            to: process.env.PAULINE_MAILER,
+            subject: req.body.subject,
+            html: `<div>${req.body.message}</div><div><p> email envoyé par :${req.body.from}</p></div>` 
+        });
+        res.status(200).send('Email sent');
+    } catch (error) {
+        
+    }
+});
---
+++ services/mails.js
@@ -0,0 +1,19 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.LOG_MAILER,
+        pass: process.env.MDP_MAILER
+    }
+});
+
+exports.sendMail = (mailoption) => {
+    transporter.sendMail(mailoption, (err, info) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    })
+}
